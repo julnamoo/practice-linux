21fc4d8f16690e23765b9fdf758060db58a561c9
CentOS5.8 <kjulee114@gmail.com>
2012-10-02 10:53:41 -0700

    finish list1422

--- ex14/list1422/list1422.c
+++ ex14/list1422/list1422.c
@@ -7,5 +7,43 @@
 int main(argc, char* argv[]) {
   HIDInterface* hid = NULL;
   int if_no = 0;
+  HIDInterfaceMatcher matcher = { VENDOR_ID, PRODUCT_ID, NULL, NULL, 0 };
+  
+  /* init */
+  if (hid_init() != 0) {
+    fprintf(stderr, "Cannot initialize HID Library\n");
+    return 1;
+  }
 
+  /* create interface */
+  if ((hid = hid_new_HIDInterface()) == NULL) {
+    fprintf(stderr, "Cannot access HID interface\n");
+    return 1;
+  }
+
+  /* open device */
+  if (hid_force_open(hid, if_no, &matcher, 3) != 0) {
+    fprintf(stderr, "Cannot open HID device\n");
+    return 1;
+  }
+
+  /* print device info */
+  if (hid_write_identification(stdout, hid) != 0) {
+    fprintf(stderr, "Cannot write identification\n");
+    return 1;
+  }
+
+  /* close device */
+  if (hid_close(hid) != 0) {
+    fprintf(stderr, "Cannot close HID device\n");
+    return 2;
+  }
+
+  /* terminate */
+  if (hid_cleanup() != 0) {
+    fprintf(stderr, "Cannot cleanup HID library\n");
+    return 1;
+  }
+
+  return 0;
 }
