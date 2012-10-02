d46d2d15b4ed7bc1b73cf8b4dae435eed6579bd8
CentOS5.8 <kjulee114@gmail.com>
2012-10-02 11:09:36 -0700

    example releasing ipod devices in chapter 14

---
+++ ex14/list1432/list1432.c
@@ -0,0 +1,39 @@
+#include <libimobiledevice/libimobiledevice.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  char** devices;
+  idevice_t device;
+  int i, count;
+
+  /* get device list */
+  if (idevice_get_device_list(&devices, &count) != 0) {
+    fprintf(stderr, "Cannot get device list\n");
+    return 1;
+  }
+  printf("%d device(s) found.\n", count);
+
+  for (i = 0; i < count; ++i) {
+    /* create device */
+    if (idevice_new(&device, devices[i]) != 0) {
+      fprintf(stderr, "Cannot create device[%s]\n", devices[i]);
+      return 1;
+    }
+
+    printf("device %d [%s] is ready\n", i, devices[i]);
+
+    /* release device */
+    if (idevicd_free(device) != 0) {
+      fprintf(stderr, "Cannot free device[%s]\n", devices[i]);
+      return 1;
+    }
+  }
+
+  /* release device list */
+  if (idevice_free(device) != 0) {
+    fprintf(stderr, "Cannot free device list\n");
+    return 1;
+  }
+
+  return 0;
+}
