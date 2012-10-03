37262806130d3c048fe7eae8b681f36a8834dbd6
Julie <kjulee114@gmail.com>
2012-10-03 13:23:30 +0900

    step4 for convex hull, change background to white

--- ex17/cvxfield.c
+++ ex17/cvxfield.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "cvxfield.h"
 
+static gboolean cvx_field_expose_event(GtkWidget* widget,
+    GdkEventExpose event, gpointer user_data) {
+  g_print("exposed!\n");
+  return FALSE;
+}
+
 CvxField* cvx_field_new(GtkWindow* window,
     gchar* title, guint width, guint height) {
   CvxField* retvar;
@@ -10,6 +16,12 @@ CvxField* cvx_field_new(GtkWindow* window,
   if (retvar == NULL) return NULL;
   gtk_window_set_title(window, title);
   gtk_widget_set_size_request(GTK_WIDGET(window), width, height);
+  retvar->window = window;
+
+  canvas = gtk_drawing_area_new();
+  gtk_container_add(GTK_CONTAINER(window), canvas);
+  g_signal_connect(GTK_OBJECT(window), "expose-event",
+      G_CALLBACK(cvx_field_expose_event), retvar);
   retvar->canvas = canvas;
 
   return retvar;
--- ex17/main.c
+++ ex17/main.c
@@ -3,6 +3,7 @@
 #define CVX_MAIN_TITLE  "main_window"
 #define CVX_MAIN_WIDTH  400
 #define CVX_MAIN_HEIGHT 300
+#define CVX_MAIN_BGCOLOR "White"
 
 int main(int argc, char* argv[]) {
   GtkWidget* window;
@@ -17,6 +18,16 @@ int main(int argc, char* argv[]) {
   field = cvx_field_new(GTK_WINDOW(window),
       CVX_MAIN_TITLE, CVX_MAIN_WIDTH, CVX_MAIN_HEIGHT);
   gtk_widget_show_all(window);
+
+  {
+    GdkColor color;
+    GdkScreen* screen;
+
+    gdk_color_parse(CVX_MAIN_BGCOLOR, &color);
+    screen = gdk_display_get_default_screen(gdk_display_get_default());
+    gdk_rgb_find_color(gdk_screen_get_rgb_colormap(screen), &color);
+    gdk_window_set_background(field->canvas->window, &color);
+  }
 
   gtk_main();
   return 0;
