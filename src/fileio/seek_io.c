88bffc19558a1164ce2816b545780fe8c87e36be
CentOS5.8 <kjulee114@gmail.com>
2012-10-29 06:16:34 -0700

    simple program using read and write with lseek, seek_io. tfile is results of it

---
+++ src/fileio/seek_io.c
@@ -0,0 +1,76 @@
+// Julie Kim (julie114.kim@gmail.com)
+// A simple program using read(), write() with lseek().
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include "../lib/tlpi_hdr.h"
+
+int main(int argc, char* argv[]) {
+  size_t len;
+  off_t offset;
+  int fd, ap, j;
+  char *buf;
+  ssize_t numRead, numWritten;
+
+  if (argc < 3 || strcmp(argv[1], "--help") == 0) {
+    usageErr("%s file {r<length>|R<length>|w<string>|s<offet>}...\n", argv[0]);
+  }
+
+  /* rw-rw-rw- */
+  fd = open(argv[1], O_RDWR|O_CREAT, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|
+      S_IROTH|S_IWOTH);
+  if (fd == -1) {
+    errExit("open");
+  }
+
+  for (ap = 2; ap < argc; ap++) {
+    switch (argv[ap][0]) {
+      case 'r': /* print text from current offset */
+      case 'R': /* print hexadecimals from current offset */
+        len = getLong(&argv[ap][1], GN_ANY_BASE, argv[ap]);
+        buf = malloc(len);
+        if (buf == NULL) {
+          errExit("malloc");
+        }
+
+        numRead = read(fd, buf, len);
+        if (numRead == -1) {
+          errExit("read");
+        }
+        if (numRead == 0) {
+          printf("%s: end-of-file\n", argv[ap]);
+        } else {
+          printf("%s: ", argv[ap]);
+          for (j = 0; j < numRead; j++) {
+            if (argv[ap][0] == 'r') {
+              printf("%c", isprint((unsigned char) buf[j]) ? buf[j] : '?');
+            } else {
+              printf("%02x ", (unsigned int) buf[j]);
+            }
+          }
+          printf("\n");
+        }
+        free(buf);
+        break;
+      case 'w': /* write characters */
+        numWritten = write(fd, &argv[ap][1], strlen(&argv[ap][1]));
+        if (numWritten == -1) {
+          errExit("write");
+        }
+        printf("%s: wrote %ld byes\n", argv[ap], (long) numWritten);
+        break;
+
+      case 's': /* change file offset */
+        offset = getLong(&argv[ap][1], GN_ANY_BASE, argv[ap]);
+        if (lseek(fd, offset, SEEK_SET) == -1) {
+          errExit("lseek");
+        }
+        printf("%s: seek succeeded\n", argv[ap]);
+        break;
+      default:
+        cmdLineErr("Argument must start with [rRws]: %s\n", argv[ap]);
+    }
+  }
+
+  exit(EXIT_SUCCESS);
+}
