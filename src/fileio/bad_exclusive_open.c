5ae9b880425d65969efbd04ae0a89c85f5510274
CentOS5.8 <kjulee114@gmail.com>
2012-10-30 09:56:26 -0700

    bad exclusive open causing deadlock for opening file.--version1

---
+++ src/fileio/bad_exclusive_open.c
@@ -0,0 +1,33 @@
+// Julie Kim (julie114.kim@gmail.com)
+// A simple program using open() systemcall.
+// If there are one more processes runinng it, they are can be on deadlock.
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "../lib/tlpi_hdr.h"
+
+int main(int argc, char** argv) {
+  int fd;
+
+  if (argc < 2 || strcmp(argv[1], "--help") == 0) {
+    usageErr("%s file\n", argv[0]);
+  }
+
+  fd = open(argv[1], O_WRONLY); 
+  if (fd == -1) {
+    printf("[PID %ld] File \"%s\" already exists\n", (long) getpid(), argv[1]);
+    close(fd);
+  } else {
+    if (errno != ENOENT) { /* cannot finish the job with unknwon error */
+      errExit("open");
+    } else {
+      /* probability fail point */
+      fd = open(argv[1], O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+      if (fd == -1) {
+        errExit("open");
+      }
+
+      printf("[PID %ls] Created file \"%s\" exclusively\n",
+          (long) getpid(), argv[1]); /* It may be not true */
+    }
+  }
+}
