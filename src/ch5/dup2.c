862aa0e06be589acaca28a79355e9d156890741a
CentOS5.8 <kjulee114@gmail.com>
2012-11-02 00:20:40 -0700

    program like dup2

---
+++ src/ch5/dup2.c
@@ -0,0 +1,26 @@
+// Julie Kim (julie114.kim@gmail.com)
+// Probrem5-3.2.Program mimics dup2() with fcntl() and close()
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include "../lib/tlpi_hdr.h"
+
+int main(int argc, char** argv) {
+  int newfd, tempfd;
+
+  if (argc != 3 || strcmp(argv[1], "--help") == 0) {
+    usageErr("%s oldfd newfd\n", argv[0]);
+  }
+
+  tempfd = atoi(argv[2]);
+  // Check newFd is available
+  if (fcntl(tempfd, F_GETFD) > 0) {
+    if (close(tempfd) == -1) {
+      errExit("close opened fd as newfd");
+    }
+  }
+  newfd = fcntl(atoi(argv[1]), F_DUPFD, tempfd);
+  printf("copied fd:%d\n", newfd);
+  write(newfd, "hello", 5);
+  return newfd;
+}
