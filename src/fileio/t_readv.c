cacf84185ec81e6c28a276f774cdcb0f66fadf8a
CentOS5.8 <kjulee114@gmail.com>
2012-11-02 02:25:17 -0700

    modify to adjust iov elements

--- src/fileio/t_readv.c
+++ src/fileio/t_readv.c
@@ -28,12 +28,12 @@ int main(int argc, char** argv) {
   iov[0].iov_len = sizeof(struct stat);
   totRequired += iov[0].iov_len;
 
-  iov[1].iov_base = &myStruct;
-  iov[1].iov_len = sizeof(struct stat);
+  iov[1].iov_base = &x;
+  iov[1].iov_len = sizeof(x);
   totRequired += iov[1].iov_len;
 
-  iov[2].iov_base = &myStruct;
-  iov[2].iov_len = sizeof(struct stat);
+  iov[2].iov_base = str;
+  iov[2].iov_len =STR_SIZE;
   totRequired += iov[2].iov_len;
 
   numRead = readv(fd, iov, 3);
